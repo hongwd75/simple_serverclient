bcf35b4f2928ac2bdc52b6b9ec4d12e3579ad340
HongWD <choksae@nm-n2.com>
2024-11-15 18:40:02 +0900

    유니티 클라이언트 준비

--- common/fbs/run.bat
+++ common/fbs/run.bat
@@ -1,10 +1,18 @@
 flatc 	--gen-object-api --object-prefix "" --object-suffix "_FBS" --force-empty ^
-	 --cpp --cpp-ptr-type std::shared_ptr --gen-name-strings ^
+	--cpp --cpp-ptr-type std::shared_ptr --gen-name-strings ^
 	-o cpp ^
 	--gen-compare ^
 	enumPackets.fbs ^
 	structs.fbs ^
 	networkmessages.fbs 
 
+
+flatc 	--gen-object-api --object-prefix "" --object-suffix "_FBS" --force-empty ^
+	--csharp --gen-onefile ^
+	-o csharp ^
+	--gen-compare ^
+	enumPackets.fbs ^
+	structs.fbs ^
+	networkmessages.fbs 
 pause
 
---
+++ unity/viewer/Assets/Scripts/Utitlity/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utitlity
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        public Transform target; // 따라갈 대상
+        public Vector3 offset = new Vector3(0, 66, -66); // 카메라와 대상 사이의 거리
+        public float smoothSpeed = 0.125f; // 카메라 이동 속도
+
+        void LateUpdate()
+        {
+            if (target == true)
+            {
+                Vector3 desiredPosition = target.position + offset;
+                Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+                transform.position = smoothedPosition;
+
+                transform.LookAt(target);
+            }
+        }        
+    }
+}
