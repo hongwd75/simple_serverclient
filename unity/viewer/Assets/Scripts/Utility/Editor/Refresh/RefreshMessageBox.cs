722117fc092075117dfab862b1045d8d55996786
HongWD <choksae@nm-n2.com>
2025-01-02 09:43:14 +0900

    코드 업데이트

--- unity/viewer/Assets/Scripts/Utility/Editor/Refresh/RefreshMessageBox.cs
+++ unity/viewer/Assets/Scripts/Utility/Editor/Refresh/RefreshMessageBox.cs
@@ -1,7 +1,45 @@
-﻿namespace Utility.Editor.Refresh
+﻿using System;
+using UnityEditor;
+/*
+ *  사용법
+ *  using(RefreshMessageBox.RefreshNow(true))
+ *  {
+ *    에디터 박스 사용
+ *  }
+ * 
+ */
+namespace Utility.Editor.Refresh
 {
-    public class RefreshMessageBox
+    public struct RefreshMessageBox : IDisposable
     {
+        // 설정 값 저장
+        private bool savedSetting;
+        private bool refreshAsset;
+
+        public static RefreshMessageBox RefrshNow(bool forceRefresh)
+        {
+            return new RefreshMessageBox(forceRefresh);
+        }
         
+        private RefreshMessageBox(bool forceRefresh)
+        {
+            savedSetting = EditorUserSettings.AutomaticAdd;
+            refreshAsset = forceRefresh;
+            EditorUserSettings.AutomaticAdd = false;
+            AssetDatabase.StartAssetEditing();
+            EditorApplication.LockReloadAssemblies();
+        }
+        
+        public void Dispose()
+        {
+            EditorApplication.UnlockReloadAssemblies();
+            AssetDatabase.StopAssetEditing();
+
+            if (refreshAsset == true)
+            {
+                AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+            }
+            EditorUserSettings.AutomaticAdd = savedSetting;
+        }
     }
 }
